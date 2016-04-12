4c4955c2159373b01b114f3ee922a3e40a66586c
Ivan Molina <ivanmol@users.noreply.github.com>
2016-04-12 11:38:43 +0100

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 // Declare app level module which depends on filters, and services
 angular.module('popupEngine.config', [])
 	
-	.constant('FBURL', 'https://kupopups.firebaseio.com/')
+	.constant('FBURL', 'https://<YOUR-FIREBASE-APP>.firebaseio.com/')
 
 	// where to redirect users if they need to authenticate (see security.js)
 	.constant('loginRedirectPath', '/login');
